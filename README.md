250dd70472bb514533d8e1beb356c89652ea96c5
Sodiq Alabi <adisco4420@gmail.com>
2022-06-07 19:13:18 +0100

    allow passing of baseUrl into params, fixed vulnerabilities issues

--- README.md
+++ README.md
@@ -9,22 +9,25 @@ NodeJS wrapper for making secure request to Kuda API
 
 ## Using the library
 
-run `npm install kuda-node`
+run `npm install swift-kuda-node`
 
 ### Library setup
 
 ```js
 const fs = require("fs");
-const Kuda = require('kuda-node'); 
+const Kuda = require('swift-kuda-node'); 
 
 const publicKey = fs.readFileSync("./kuda.public.xml"); // or path to your kuda public key
 const privateKey = fs.readFileSync("./path-to-private-key.xml"); // or path to your kuda kuda private key
 const clientKey = "name-of-private-key-file"; // name of private key file without the .xml suffix (extension)
+const baseUrl = 'https://kuda-openapi.kuda.com/v1​';
+
 
 const kuda = Kuda({
   publicKey,
   privateKey,
-  clientKey
+  clientKey,
+  baseUrl
 }); // this initialize the Kuda function
 ```
 
@@ -46,29 +49,10 @@ kuda({
 // account creation
 const shortid = require("shortid"); // this libarary will generate random id for you. You can install with `yarn add shortid` or `npm i shortid`. You can use any other random key generatring library of your choice
 
-kuda(
-  {
-    serviceType: "CREATE_VIRTUAL_ACCOUNT",
-    requestRef: Math.floor(Math.random() * 1000000000000 + 1), // you can generate your random number your own way. This is just an example.
-    data: {
-      email: "ajala@obi.com",
-      phoneNumber: "08012345678",
-      firstName: "Ajala",
-      lastName: "Obi",
-      trackingReference: "vAcc-" + shortid.generate() // you can generate your trackingReference some other way you choose.
-    }
-  },
-  data => {
-    // data => decrypted response from Kuda API
-    // do anything with your data
-    console.log(JSON.stringify(data, null, 2));
-  }
-);
-
 // it can also be called with in an async await fashion like so
 const onboardUser = async(email, phoneNumber, firstName, lastName, trackingReference) => {
   const response = await kuda({
-    serviceType: 'CREATE_VIRTUAL_ACCOUNT',
+    serviceType: 'ADMIN_CREATE_VIRTUAL_ACCOUNT',
     requestRef: Number, // like Math.floor(Math.random() * 1000000000000 + 1)
     data: { email, phoneNumber, firstName, lastName, trackingReference }
   })
@@ -85,6 +69,7 @@ const onboardUser = async(email, phoneNumber, firstName, lastName, trackingRefer
 
 ## Authors
 
+- [Sodiq Alabi](https://github.com/adisco4420)
 - [Ajala Abdulsamii](https://codementor.io/jalasem)
 - [Abdulazeez Murainah](https://github.com/gceezle)
 - [Azeez Adio](https://github.com/azeezadio)
--- index.js
+++ index.js
@@ -7,74 +7,73 @@ const { RSAEncrypt, RSADecrypt } = require('./algorithms/rsa')
 
 /**
  * @description initialize the Kuda wrapper function
- * @param {object} param => publicKeyPath, privateKeyPath, clientKey
+ * @param {object} param => publicKeyPath, privateKeyPath, clientKey, baseUrl
  * @return {function} request function
  */
 function Kuda (param) {
-  if (!param) return console.log('Error: publicKey, privateKey, clientKey are required!')
+  if (!param) return console.log('Error: publicKey, privateKey, clientKey, baseUrl are required!')
 
-  let { publicKey, privateKey } = param
+  let { publicKey, privateKey, baseUrl } = param
   publicKey = publicKey.toString()
   privateKey = privateKey.toString()
 
   const { clientKey } = param
-  
   if (!publicKey) return console.log('Error: publicKey is required!')
   if (!privateKey) return console.log('Error: privateKey is required!')
   if (!clientKey) return console.log('Error: clientKey is required!')
-  
+  if (!baseUrl) return console.log('Error: baseUrl is required!')
+
   const password = `${clientKey}-${shortid.generate().substring(0, 5)}`
 
   /**
    * makes an encrypted call to Kuda API
    * @param {object} params => serviceType, requestRef, data
-   * @param {function} callback gets called with the result(data) object
    * @return {object} data return decrypted data response object
    */
-  async function request (params, callback) {
-    if (!params) return console.log('Error: serviceType, requestRef and data are required!')
+  async function request (params) {
+    return new Promise(async (resolve, reject) => {
+      if (!params) return console.log('Error: serviceType, requestRef and data are required!')
 
-    const { serviceType, requestRef, data } = params
+      const { serviceType, requestRef, data } = params
 
-    const payload = JSON.stringify({
-      serviceType,
-      requestRef,
-      data
-    })
-
-    try {
-      // aes encrypt payload with password
-      const encryptedPayload = await aesEncrypt(payload, password)
-      // rsa encrypt password with public key
-      const encryptedPassword = await RSAEncrypt(password, publicKey)
-
-      // make encrypted api request to Kuda Bank
-      const { data: encryptedResponse } = await axios.post('https://kuda-openapi.kuda.com/v1', {
-        data: encryptedPayload
-      }, {
-        headers: {
-          password: encryptedPassword
-        }
+      const payload = JSON.stringify({
+        serviceType,
+        requestRef,
+        data
       })
 
-      // plaintext = RSA decrypt password with our privateKey
-      const plaintext = await RSADecrypt(encryptedResponse.password, privateKey).toString()
-      // data = AES decrypt data with plaintext
-      let data = await aesDecrypt(encryptedResponse.data, plaintext)
-      if (typeof data === 'string') data = JSON.parse(data)
+      try {
+        // aes encrypt payload with password
+        const encryptedPayload = await aesEncrypt(payload, password)
+        // rsa encrypt password with public key
+        const encryptedPassword = await RSAEncrypt(password, publicKey)
 
-      // console.log('encrypted response:', JSON.stringify({
-      //   encryptedResponse,
-      //   plaintext,
-      //   data: JSON.parse(data)
-      // }, null, 2))
+        // make encrypted api request to Kuda Bank
+        const { data: encryptedResponse } = await axios.post(baseUrl, {
+          data: encryptedPayload
+        }, {
+          headers: {
+            password: encryptedPassword
+          }
+        })
 
-      if (callback) return callback(data)
+        // plaintext = RSA decrypt password with our privateKey
+        const plaintext = await RSADecrypt(encryptedResponse.password, privateKey).toString()
+        // data = AES decrypt data with plaintext
+        let data = await aesDecrypt(encryptedResponse.data, plaintext)
+        if (typeof data === 'string') data = JSON.parse(data)
 
-      return data
-    } catch (err) {
-      console.log(err.stack)
-    }
+        // console.log('encrypted response:', JSON.stringify({
+        //   encryptedResponse,
+        //   plaintext,
+        //   data: JSON.parse(data)
+        // }, null, 2))
+
+        resolve(data)
+      } catch (err) {
+        reject(err)
+      }
+    })
   }
 
   return request
